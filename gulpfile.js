a4218e866d06c73c1f66cb97335de75e72315655
miraidesigns <ericseyfert92@gmail.com>
2021-07-13 16:26:49 +0200

    Update tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,74 +9,67 @@ const buffer = require('vinyl-buffer');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify-es').default;
 
-gulp.task('scss', function () {
-    return gulp.src('./modules/app.scss')
-        .pipe(scss())
-        .pipe(autoprefixer('last 5 version'))
-        .pipe(gulp.dest('./'));
+gulp.task('scss-dirty', function () {
+	return gulp
+		.src('app.scss')
+		.pipe(scss({ includePaths: ['node_modules'] }))
+		.pipe(autoprefixer('last 5 version'))
+		.pipe(gulp.dest('/'));
 });
 
 gulp.task('scss-clean', function () {
-    return gulp.src('./modules/app.scss')
-        .pipe(scss())
-        .pipe(autoprefixer('last 5 version'))
-        .pipe(cleanCSS({
-            rebase: false,
-            level: {
-                2: {
-                    restructureRules: true,
-                    mergeSemantically: true
-                }
-            }
-        }))
-        .pipe(gulp.dest('./'));
+	return gulp
+		.src('app.scss')
+		.pipe(scss({ includePaths: ['node_modules'] }))
+		.pipe(autoprefixer('last 5 version'))
+		.pipe(
+			cleanCSS({
+				rebase: false,
+				level: {
+					1: {
+						specialComments: 0,
+					},
+					2: {
+						restructureRules: true,
+						mergeSemantically: true,
+					},
+				},
+			})
+		)
+		.pipe(gulp.dest('/'));
 });
 
 gulp.task('ts', function () {
-    return browserify({
-        entries: './modules/app.ts',
-        extensions: ['.ts'],
-        debug: true,
-    })
-        .plugin(tsify)
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('ts-babel', function () {
-    return browserify({
-        entries: './modules/app.ts',
-        extensions: ['.ts'],
-        debug: true,
-    })
-        .plugin(tsify)
-        .transform('babelify', {
-            presets: ['@babel/env'],
-            plugins: [
-                [
-                    '@babel/plugin-transform-runtime',
-                    {
-                        corejs: '3',
-                    },
-                ],
-            ],
-            extensions: ['.ts'],
-        })
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('/'))
-        .pipe(gulp.dest('./'));
+	return browserify({
+		entries: 'app.ts',
+		extensions: ['.ts'],
+		debug: true,
+	})
+		.plugin(tsify)
+		.transform('babelify', {
+			presets: ['@babel/env', '@babel/preset-typescript'],
+			plugins: [
+				[
+					'@babel/plugin-transform-runtime',
+					{
+						corejs: '3',
+					},
+				],
+			],
+			extensions: ['.ts'],
+			sourceMaps: true,
+			global: true,
+		})
+		.bundle()
+		.pipe(source('app.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest('/'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./modules/**/*.scss', gulp.series('scss'));
-    gulp.watch('./modules/**/*.ts', gulp.series('ts'));
+	gulp.watch('./modules/**/*.scss', gulp.series('scss-dirty'));
+	gulp.watch('./modules/**/*.ts', gulp.series('ts'));
 });
