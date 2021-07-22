2f9cb1dc8cef2f3db3d06aa790e086dce8c3dc1c
miraidesigns <ericseyfert92@gmail.com>
2021-07-22 17:23:45 +0200

    Update code to await the loading of images

--- modules/grid/mdf-grid.ts
+++ modules/grid/mdf-grid.ts
@@ -1,5 +1,5 @@
 import '@miraidesigns/base';
-import { classes, selectors } from './constants';
+import { selectors } from './constants';
 
 /**
  * MDFGrid
@@ -8,13 +8,14 @@ import { classes, selectors } from './constants';
  *
  * @export
  * @class MDFGrid
- * @version 1.0.1
+ * @version 1.0.3
  */
 export class MDFGrid {
 	public readonly container: HTMLElement;
 	public readonly items: HTMLElement[];
 
 	private autoRows: number;
+	private images: HTMLCollection;
 	private rowGap: number;
 
 	/**
@@ -23,6 +24,7 @@ export class MDFGrid {
 	 * @param {Element} elem The element we are manipulating
 	 * @memberof MDFGrid
 	 * @since 1.0.0
+	 * @version 1.0.3
 	 */
 	constructor(elem: Element) {
 		// If the supplied element doesn't exist, abort the script.
@@ -37,12 +39,12 @@ export class MDFGrid {
 		// If we have no grid items, abort the script.
 		if (!this.items) return;
 
+		// Get all images inside our .
+		this.images = this.container.getElementsByTagName('img');
+
 		// Get CSSStyle properties for the grid.
 		this.autoRows = parseInt(window.getComputedStyle(this.container).getPropertyValue('grid-auto-rows'));
 		this.rowGap = parseInt(window.getComputedStyle(this.container).getPropertyValue('grid-row-gap'));
-
-		// Set the initial state of our elements.
-		this.setInitialState();
 	}
 
 	/**
@@ -52,11 +54,69 @@ export class MDFGrid {
 	 *
 	 * @memberof MDFGrid
 	 * @since 1.0.0
+	 * @version 1.0.3
 	 */
-	public resize = (): void => {
-		for (const item of this.items) {
-			this.resizeItem(item);
+	public resize = async (): Promise<void> => {
+		// Check wether or not we have images in our grid.
+		if (this.images.length) {
+			// We temporarily hide the grid while we wait for the images to finish loading.
+			this.container.style.opacity = '0';
+
+			// Status of our images.
+			const imagesLoaded = await this.waitForImages();
+
+			if (imagesLoaded) {
+				// Once the images are loaded, we resize our items.
+				for (const item of this.items) {
+					this.resizeItem(item);
+				}
+
+				// And we make our grid visible again.
+				this.container.style.opacity = '1';
+			}
+		} else {
+			for (const item of this.items) {
+				this.resizeItem(item);
+			}
 		}
+	};
+
+	/**
+	 * waitForImages
+	 *
+	 * Wait for all images inside the grid to load.
+	 *
+	 * @private
+	 * @memberof MDFGrid
+	 * @since 1.0.3
+	 */
+	private waitForImages = () => {
+		return new Promise<boolean>((resolve) => {
+			// We use this later in our resolve to let the script know the images have been loaded.
+			let isLoaded = false;
+
+			// We use this value to keep track of how many images have been loaded.
+			let imageCount = 0;
+
+			// Loop through all images.
+			for (const image of this.images) {
+				// Create a clone of the current image without adding it to the DOM.
+				const clone = new Image();
+				clone.src = (image as HTMLImageElement).src;
+
+				// Wait for the cloned image to be fully loaded.
+				void clone.decode().then(() => {
+					// Increase the images loaded counter.
+					imageCount++;
+
+					// Once the counter is equal to the amount of images, we resolve the promise.
+					if (imageCount === this.images.length) {
+						isLoaded = true;
+						resolve(isLoaded);
+					}
+				});
+			}
+		});
 	};
 
 	/**
@@ -84,45 +144,16 @@ export class MDFGrid {
 	 * @param {HTMLElement} item The item we are resizing
 	 * @memberof MDFGrid
 	 * @since 1.0.0
-	 * @version 1.0.1
+	 * @version 1.0.3
 	 */
 	private resizeItem = (item: HTMLElement) => {
 		// Make sure the item has the content wrapper element in place.
 		if (item.firstElementChild.matches(selectors.itemContent)) {
-			// Get all <img> elements.
-			const images = item.getElementsByTagName('img');
+			// Calculate span.
+			const span = this.calcSpan(item);
 
-			// Check if we have an image.
-			if (images[0]) {
-				// Wait for the image to load.
-				images[0].onload = () => {
-					// Calculate span.
-					const span = this.calcSpan(item);
-
-					// Set the span (height) of the grid item.
-					item.style.gridRowEnd = `span ${span}`;
-				};
-			} else {
-				// Calculate span.
-				const span = this.calcSpan(item);
-
-				// Set the span (height) of the grid item.
-				item.style.gridRowEnd = `span ${span}`;
-			}
+			// Set the span (height) of the grid item.
+			item.style.gridRowEnd = `span ${span}`;
 		}
-	};
-
-	/**
-	 * setInitialState
-	 *
-	 * Make sure the grid has the proper attributes on page load.
-	 *
-	 * @private
-	 * @memberof MDFGrid
-	 * @since 1.0.0
-	 */
-	private setInitialState = () => {
-		// Make sure the grid has the masonry class.
-		this.container.addClass(classes.masonry);
 	};
 }
