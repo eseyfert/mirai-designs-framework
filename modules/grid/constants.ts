99f230d4087a76ac8a88082537f65a34be8a41a6
miraidesigns <ericseyfert92@gmail.com>
2021-07-22 17:23:20 +0200

    Remove unused values

--- modules/grid/constants.ts
+++ modules/grid/constants.ts
@@ -1,11 +1,7 @@
-const classes = {
-	masonry: 'mdf-grid--masonry',
-};
-
 const selectors = {
 	grid: '.mdf-grid',
 	item: '.mdf-grid__item',
 	itemContent: '.mdf-grid__item-content',
 };
 
-export { classes, selectors };
+export { selectors };
